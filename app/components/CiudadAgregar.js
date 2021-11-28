e0adc902fb7ac00d26eb47517f678848f462031a
Cristian_M <clm.833@gmail.com>
2021-11-28 00:23:57 -0300

    Estilos en cards de resultados de busqueda

--- app/components/CiudadAgregar.js
+++ app/components/CiudadAgregar.js
@@ -10,35 +10,31 @@ const CiudadAgregar = ({
 }) => {
   return (
     <TouchableOpacity onPress={onPress, onIconPress} style={[styles.item, backgroundColor]}>
-        <View style={styles.view_content}>
-          <Text style={[styles.title, textColor]}>
-            {item.name} ({item.country})
-          </Text>
-          {item.icon && item.temp ? (
-          <View style={styles.weather_row}>
-            <View>
-              <Image
-                style={{width: 40, height: 50}}
-                source={{
-                  uri: `http://openweathermap.org/img/w/${item.icon}.png`,
-                }}
-              />
-            </View>
-            <View style={[styles.weather_row, {paddingLeft: 8}]}>
-              <Text style={styles.weather_detail}>
-                {parseInt(item.temp - 273.15)} &#x2103;
-              </Text>
-            </View>
-          </View>
+      <View style={styles.view_content}>
+        <Text style={[styles.title, textColor]}>
+          {item.name} ({item.country})
+        </Text>
+        {item.icon && item.temp ? (
+          <>
+            <Image
+              style={styles.imgWeather}
+              source={{
+                uri: `http://openweathermap.org/img/w/${item.icon}.png`,
+              }}
+            />
+            <Text style={styles.temp}>
+              {parseInt(item.temp - 273.15)} &#x2103;
+            </Text>
+          </>
         ) : (
-          <View>
+          <>
             <Text style={[styles.detail, textColor]}>ID: {item.id}</Text>
             <Text style={[styles.detail, textColor]}>
               Lat: {item.lat} - Lon: {item.lon}
             </Text>
-          </View>
+          </>
         )}
-        </View>
+      </View>
     </TouchableOpacity>
   );
 };
@@ -50,22 +46,29 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   view_content: {
-    // width: '90%',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   title: {
+    flex: 1,
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  imgWeather: {
+    alignSelf: 'center',
+    marginTop: -8,
+    marginBottom: -8,
+    marginHorizontal: 8,
+    width: 50,
+    height: 50,
   },
   detail: {
     fontSize: 12,
   },
-  weather_row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  weather_detail: {
-    fontSize: 16,
+  temp: {
+    fontSize: 22,
     fontWeight: 'bold',
+    color: '#eee',
   },
 });
 
